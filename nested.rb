f7fd2ee8859082b8fecba7e99982722903f80ec9
Jose Chavira <joeychavira2016@gmail.com>
2019-08-30 22:02:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,10 +16,8 @@ def hopper
         }
      }
 		programmer_hash={
-        :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
-        }
 			}
 
 end
